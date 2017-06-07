29ffde3d8daeafce8d1d1b6f08cc67741ec554dd
RamanSaini1 <raman.11.27s@gmail.com>
2017-06-07 18:03:24 -0400

    Update PlayerList.java

--- Pong/src/PlayerList.java
+++ Pong/src/PlayerList.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
 
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
@@ -254,6 +255,214 @@ public class PlayerList {
         //Self-Testing Main
         static void main (String args[])
         {
-        	
+            //create an object of the accountList class
+            PlayerList playerList = new PlayerList();
+
+            //variable for selected option
+            String choice = ""; 
+
+            //array to hold the different options
+            String [] choices = {"Insert", "Sort", "Binary Search", "Load From File", "Write to File",  "Print", "Delete", "Quit"};
+
+            while(true) //Infinite While Loop
+            {
+                    //Choose A Method To Test
+                    choice = (String) JOptionPane.showInputDialog (null, "Choose A Method To Test", 
+                                    "Job", JOptionPane.PLAIN_MESSAGE, null, choices, "Insert");
+
+                    if (choice.equals("Quit")) //Quit
+                    {
+                    	break;
+                    }
+                    //Switch Case
+                    switch(choice)
+                    {
+                    case "Insert": //Insert Information
+                    {
+                    	//variable for the record
+                        String record = JOptionPane.showInputDialog(null, "Enter, <User Name>,<Wins>,<Losses>,<Win %> <Tokens>,"
+                                        +  "Raman_2ESY, 5000, 25, 95, 15");
+
+                        record += ";0;0";
+
+                        //PlayerRecord Object
+                        PlayerRecord testInfo = new PlayerRecord();
+
+                        //separate record into different portions of data
+                        testInfo.process(record);
+
+                        //Insert If Space
+                        if (!playerList.insert(testInfo))
+                        {
+                        	JOptionPane.showMessageDialog(null, "Not Added");
+                        }
+                        else
+                        {
+                        	JOptionPane.showMessageDialog(null, "Success");
+                        }
+                        break;
+                    }
+                    case "Print":
+                    {
+                    	//Text Area For Output
+                        JTextArea display = new JTextArea(20,90);
+                        JScrollPane scroller = new JScrollPane(display);
+                        String text = "";
+
+                        text += "Record #" + "\t" + "User Name" + "\t" + "Wins" + "\t" + "Losses" + "\t" + "Win %" + "\t" + "\n" + "Tokens";
+
+                        //Loop Through And Display Info
+                        for (int i = 0; i<playerList.getSize();i++)
+                        {
+                        	text += "Record #" + (i) + "\t" +  playerList.getList()[i].getUserName() + "\t" 
+                                    + playerList.getList()[i].getUserName() + "\t" 
+                                    + playerList.getList()[i].getWins() + "\t"
+                                    + playerList.getList()[i].getLosses() + "\t"
+                                    + playerList.getList()[i].getWinPercentage() + "\t"
+                                    + playerList.getList()[i].getTokens() + "\n";
+                        }
+
+                        //set the text of the output to the text string and display editing
+                        display.setText(text); 
+
+                        //Set Size And Disable Editing
+                        display.setTabSize(15);
+                        display.setEditable(false);
+
+                        //Display The Output
+                        JOptionPane.showMessageDialog(null, scroller, null, JOptionPane.PLAIN_MESSAGE);
+                        break;
+                    }
+                    case "Delete": //Delete
+                    {
+                    	//Sort In Alphabetical Order 
+                        playerList.bubbleSortUser();
+
+                        //Variable For Record
+                        String record = JOptionPane.showInputDialog(null, "Enter, User Name>,<Wins>,<Losses>,<Win %>,"
+                                        + "<Tokens>",
+                                        "Raman_2ESY, 5000, 25, 95, 15");
+
+                        //PlayerRecord Object
+                        PlayerRecord testInfo = new PlayerRecord();
+
+                        testInfo.process(record); //put it into array of data
+
+                        if (!playerList.delete(testInfo))
+                        {
+                        	JOptionPane.showMessageDialog(null, "Item Not Found");
+                        }
+                        else
+                        {
+                        	JOptionPane.showMessageDialog(null, "Success");
+                        }
+                        break;
+                    }
+                    case "Sort": //Sort
+                    {
+                    	//Variable For Sort Order Choice
+                        String chosenSortOrder = ""; 
+
+                        //Variable For Sort Type Choice
+                        String selectedSortType = ""; 
+
+                        //Array For Sort Order Choices
+                        String [] sortOrder = {"Top", "Bottom"};
+
+                        //Array For Sort Type Choices
+                        String [] sortType = {"Name", "Wins", "Losses", "Win Percentage", "Tokens"};
+
+                        //Prompt For Sort Order
+                        chosenSortOrder = (String) JOptionPane.showInputDialog (null, "Choose Sort Order", 
+                                        "Sort Order", JOptionPane.PLAIN_MESSAGE, null, sortOrder, "Up");
+
+                        //Prompt For Sort Type
+                        selectedSortType = (String) JOptionPane.showInputDialog (null, "Choose Sort Type", 
+                                        "Sort Type", JOptionPane.PLAIN_MESSAGE, null, sortType, "Name");
+
+                        //Call insertionSort
+                        playerList.insertionSort(chosenSortOrder, selectedSortType);
+
+                        //Display Success Message
+                        JOptionPane.showMessageDialog(null, "Insertion Sort Successful");
+                        break;
+                    }
+                    case "Load From File": //Load From File
+                    {
+                    	//Variable For File Name 
+                        String fileName = JOptionPane.showInputDialog(null, "Enter File Name To Read "
+                                        + "From", "test.txt");
+
+                        //Error Catching
+                        try
+                        {
+                        	playerList.loadFile(fileName);
+                            JOptionPane.showMessageDialog(null, "File Loaded Successfully");
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                        	JOptionPane.showMessageDialog (null, "File Not Loaded Successfully");
+                        }
+                        catch (IOException e)
+                        {
+                        	JOptionPane.showMessageDialog (null, "File Corrupted");
+                        }
+                        catch (Exception e)
+                        {
+                        	JOptionPane.showMessageDialog (null, "Error");
+                        }
+                        break;
+                    }
+                    case "Binary Search": //Binary Search
+                    {
+                    	//Sort Alphabetically before Searching
+                        playerList.bubbleSortUser();
+
+                        //Variable For search Key
+                        String searchKey = JOptionPane.showInputDialog(null, "Enter A User Name To Search For", "Raman_2ESY");
+
+                        //Variable For Location Of Item
+                        int location = playerList.binarySearchUser(searchKey);
+
+                        if (location >=0)
+                        {
+                        	JOptionPane.showMessageDialog(null, searchKey + "Found At Location " + location + 
+                                                "\nDetailed Information: " + playerList.getList()[location].toString());
+                        }
+                        else
+                        {
+                        	JOptionPane.showMessageDialog(null, searchKey + " Not Found. Try Again.");
+                        }
+                        break;
+                    }
+                    case "Write to File":
+                    {
+                    	//Variable For File name 
+                        String fileName = JOptionPane.showInputDialog(null, "Enter The File You Want To Write To"
+                                        ,"test.txt");
+
+                        //Error Catching
+                        try
+                        {
+                        	playerList.writeToFile(fileName);
+                            JOptionPane.showMessageDialog(null, "File Written Successfully");
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                        	JOptionPane.showMessageDialog (null, "File Not Loaded Successfully");
+                        }
+                        catch (IOException e)
+                        {
+                        	JOptionPane.showMessageDialog (null, "File Corrupted");
+                        }
+                        catch (Exception e)
+                        {
+                        	JOptionPane.showMessageDialog (null, "Error");
+                        }
+                        break;
+                    }
+                    }
+            }//End While
+            System.exit(0); //Exit
         } //End Main
 } //End Program
