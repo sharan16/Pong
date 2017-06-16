5a7e46c6fadfe9dd8fb0694ac55209b02a49295b
RamanSaini1 <raman.11.27s@gmail.com>
2017-06-16 14:07:48 -0400

    Update PlayerRecord.java

--- Pong/src/PlayerRecord.java
+++ Pong/src/PlayerRecord.java
@@ -4,11 +4,11 @@
  * Desc: PlayerRecord Class For Pong
  * Method List:
  */
-
 public class PlayerRecord {
 	
 	private String userName, passWord;
-	private int score, tokens;
+	private int wins, losses, tokens;
+	private double winPercentage;
 	
 	//Method To Set User Name
 	public void setUserName (String userName)
@@ -20,10 +20,20 @@ public class PlayerRecord {
 	{
 		this.passWord = passWord;
 	}
-	//Method To Set Score
-	public void setScore (int score)
+	//Method To Set Wins
+	public void setWins (int wins)
 	{
-		this.score = score;
+		this.wins = wins;
+	}
+	//Method To Set Wins
+	public void setLosses (int losses)
+	{
+		this.losses = losses;
+	}
+	//Method To Set Win Percentage
+	public void setWinPercentage (int winPercentage)
+	{
+		this.winPercentage = winPercentage;
 	}
 	//Method To Set Tokens 
 	public void setTokens (int tokens)
@@ -36,14 +46,22 @@ public class PlayerRecord {
 		return userName;
 	}
 	//Method To Get Pass Word
-	public String getPassword()
+	public String getPassWord()
 	{
 		return passWord;
 	}
-	//Method To Get Score 
-	public int getScore()
+	//Method To Get Wins 
+	public int getWins()
 	{
-		return score;
+		return wins;
+	}
+	public int getLosses()
+	{
+		return losses;
+	}
+	public double getWinPercentage()
+	{
+		return winPercentage;
 	}
 	//Method To Get Tokens 
 	public int getTokens()
@@ -58,17 +76,39 @@ public class PlayerRecord {
 
         this.userName = word [0];
         this.passWord = word [1];
-        this.score = Integer.parseInt(word [2]);
-        this.tokens = Integer.parseInt(word [3]);
+        this.wins = Integer.parseInt(word [2]);
+        this.losses = Integer.parseInt(word [3]);
+        this.winPercentage = Double.parseDouble(word [4]);
+        this.tokens = Integer.parseInt(word [5]);
     }
 	//Method To Return Processed Info As A String
     public String toString()
     { 
-        return getUserName() + "," + this.score + "," + this.tokens;
+        return this.userName + "," + this.passWord + "," + this.wins + "," + this.losses + "," + this.winPercentage + "," + this.tokens;
     }
     //Self-Testing Main Method
-    static void main (String args[])
+    public static void main (String args[])
     {
-    	//
+    	//Variable For Record 
+        String record  = "Raman_2ESY,pass123,50,0,100,56";
+
+        //Create An PccountRecord Object
+        PlayerRecord testRecord = new PlayerRecord();
+
+        //--[Process Record Testing]----
+        //Process The Record
+        testRecord.process(record);
+        
+        //--[toString Testing]----
+        //Display The Processed Record
+        System.out.println("Processed Record:" + testRecord.toString());
+
+        //--[Getter Method Testing]----
+        System.out.println("User Name: " + testRecord.getUserName());
+        System.out.println("Pass Word: " + testRecord.getPassWord());
+        System.out.println("Wins: " + testRecord.getWins());
+        System.out.println("Losses: " + testRecord.getLosses());
+        System.out.println("Win %: " + testRecord.getWinPercentage());
+        System.out.println("Tokens: " + testRecord.getTokens());
     } //End Main
 } // End Program
