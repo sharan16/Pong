df7b9faf5642f3a9869cbc5b0a9166dc1e8f2f08
WilliamBHuynh <williamhuynh2@gmail.com>
2017-06-18 21:40:46 -0400

    Update LoginGUI.java

--- Pong/src/LoginGUI.java
+++ Pong/src/LoginGUI.java
@@ -5,9 +5,13 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
 public class LoginGUI extends JFrame implements ActionListener{
@@ -20,18 +24,24 @@ public class LoginGUI extends JFrame implements ActionListener{
 	// declare buttons for login page
 	JButton loginBtn, backBtn, userClear, passClear;
 	Picture background;
-
-	public LoginGUI() {
+	
+	private PlayerList list;
+	PlayerRecord p[];
+	public LoginGUI(PlayerRecord p[]) {
 		super ("Login Page");// name of frame
 		setSize (1022, 776); // set size of frame
 		frame = getContentPane(); 
 		frame.setLayout (null);
 
+		this.p = p;
+		
 		// center GUI window
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 
 		Color color = new Color(121,135,172); // declare and create color
+		
+		JDialog.setDefaultLookAndFeelDecorated(true);// set jdialog look and feel
 
 		Font font = new Font ("arialbd",Font.BOLD, 28);// declare and create font
 
@@ -82,17 +92,49 @@ public class LoginGUI extends JFrame implements ActionListener{
 	public void actionPerformed (ActionEvent e){
 		if (e.getSource() == backBtn){
 			setVisible(false);
-			new PongGUI();
+			new PongGUI(this.p);
 		}
 		else if (e.getSource() == loginBtn){
-			setVisible(false);
-			new MenuGUI();
+			list = new PlayerList();
+			
+			String name = userLog.getText();
+			String pass = passLog.getText();
+			
+			String checkUser = name+","+pass+",0,0,0.0";
+			System.out.println(checkUser);
+			
+			try {
+				list.loadFile("Highscores.txt");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			int locName = list.linearSearch(name);
+			
+			int locPass = list.linearSearchPass(pass);
+			
+			if (locName < 0 || locPass < 0){
+				JOptionPane.showMessageDialog(null, "Login Information Incorrect.");
+					
+			}
+			else{
+				JOptionPane.showMessageDialog(null, "Login Sucessful.");
+				p[0] = list.getList()[locName];
+				setVisible(false);
+				new MenuGUI(this.p);
+			}			
+			
 		}
 
 	}
 	public static void main(String[] args) {
-		new LoginGUI();
+		PlayerRecord[] player=new PlayerRecord[2];
+		player[0]=new PlayerRecord("Campos","Pass",1,1,1);
+		player[1]=new PlayerRecord("Sharan","Pass",1,1,1);
+		new LoginGUI(player);
 
 	}
 
 }
+
