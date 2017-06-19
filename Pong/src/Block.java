a6960ae8146491061fc182961577c51fffa82958
WilliamBHuynh <williamhuynh2@gmail.com>
2017-06-19 12:08:57 -0400

    Update Block.java

--- Pong/src/Block.java
+++ Pong/src/Block.java
@@ -1,19 +1,30 @@
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Rectangle2D;
+/* Author: William Huynh
+ * Date: June. 2017
+ * Description: A program that inherits from the Thing class and is used as an obstacle in the harder difficulties of the game. 
+ * 				The obstacle changes the ball's projectile motion.
+ * Method List:
+ * Constructor - inherits from Thing class
+ * public int getvelY() - returns Y velocity 
+ * public void setvelY(int velY) - sets Y velocity 
+ * public void update(Block blocks[]) - updates Y velocity depending on interactions 
+ */
 
-public class Block extends Thing{
+public class Block extends Thing{ // inherits from Thing class
+	// declare variables for Y velocity and move 
 	private int velY;
 	private boolean move;
 	public Block() {
 		super();
 	}
-	Block (int x, int y, String imageFile){
+	Block (int x, int y, String imageFile){ // allow block to have an image icon and coordinates 
 		super(x,y,imageFile);
-		move=false;
+		move=false; // block set to not move
 
 	}
-	Block (int x, int y, String imageFile,boolean move){
+	Block (int x, int y, String imageFile,boolean move){ // allow block to move
 		super(x,y,imageFile);
 		this.move=move;
 		if(this.move){
@@ -26,25 +37,25 @@ public class Block extends Thing{
 		}
 	}
 
-	public int getvelY() {
+	public int getvelY() { // get Y velocity
 		return velY;
 	}
-	public void setvelY(int velY) {
+	public void setvelY(int velY) { // set Y velocity
 		this.velY = velY;
 	}
-	public void update(Block blocks[]){
+	public void update(Block blocks[]){ // update Y velocity depending on interactions 
 		if(this.move){
 			int y=super.getY();
 			if(blocks[0].getBody().intersects(this.getBody())||blocks[2].getBody().intersects(this.getBody())){
 				y+=1;
 				velY=1;
-				System.out.println("Top");
+				System.out.println("Top"); // test movement when block hits with top of game
 			}
 			else if (blocks[1].getBody().intersects(this.getBody())){
 				y-=1;
 				velY=-1;
 
-				System.out.println("Bottom");
+				System.out.println("Bottom"); // test movement when block hits with bottom of game
 			}
 
 			y=super.getY();	
